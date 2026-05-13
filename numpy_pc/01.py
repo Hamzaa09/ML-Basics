7761ec09b12cc90c2ad77b93a0c64d5ea33e7afa
Hamzaa09 <hamza036513@gmail.com>
2026-05-13 08:42:21 +0500

    first commit

---
+++ numpy_pc/01.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# a = np.random.rand()
+# a = np.reshape(np.arange(0,40), (4,10))
+# b = a.flatten()
+# c = a.transpose()
+# d = np.concatenate((b,b))
+# e = b[4:40]
+
+# a = np.random.randint(1, 100, size=(1,10))
+# b = np.where(a == 16)
+# print(a)
+# print(b)
+
+# ---------- find the deadline ------------------
+
+# today = np.datetime64('today', 'D')
+# deadline = np.datetime64('2026-12-31')
+
+# days_left = deadline - today
+# print(days_left)
